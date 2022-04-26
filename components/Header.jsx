2a9cf7474ecdb780929c5e322b0d76581edb91e3
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-26 18:42:22 +0000

    finish up the nav component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  UserCircleIcon,
+  UsersIconer,
+  MenuAlt1Icon,
+  MenuIcon,
+} from "@heroicons/react/solid";
 const Header = () => {
   return (
     <div className="sticky top-0 p-4 md:px-10 grid grid-cols-3 z-50 bg-white shadow-md ">
@@ -11,15 +18,22 @@ const Header = () => {
           objectPosition="left"
         />
       </div>
-      <div className=" flex items-center w-80 rounded-full border-2 border-gray-400">
+      <div className=" flex items-center w-80 rounded-full border-2 border-gray-300 pr-1">
         <input
           type="text"
           placeholder="Start your search"
           className="border-0 outline-none w-64 ml-6 mr-3"
         />
-        <SearchIcon className="h-6 p-1 bg-red-400 text-white rounded-full cursor-pointer" />
+        <SearchIcon className="h-8 p-2 bg-red-400 text-white rounded-full cursor-pointer" />
       </div>
-      <div>actions</div>
+      <div className="flex items-center justify-end space-x-3">
+        <span>Become a host</span>
+        <GlobeAltIcon className="h-6" />
+        <div className="flex items-center border-2 border-gray-400 rounded-full p-2">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </div>
   );
 };
